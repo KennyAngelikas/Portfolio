89cae648ad38fdce0cedb8f04947bd311624c098
KennyAngelikas <tuq24729@temple.edu>
2024-09-28 19:48:45 -0400

    everythign works, base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import './index.css';
 import './App.css';
+
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-center text-blue-500">
+      <h1 className="text-3xl font-bold underline">
+        Hello, Tailwind CSS!
+      </h1>
     </div>
   );
 }
 
-export default App;
+export default App;  // This is the default export
